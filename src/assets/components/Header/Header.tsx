38f445a9a3afcae4affd9c81af0b2bc2f772b141
Marcos Sugai Ogama <pazzino2000@yahoo.it>
2023-03-03 17:04:37 -0300

    init 20230303

--- src/assets/components/Header/Header.tsx
+++ src/assets/components/Header/Header.tsx
@@ -5,28 +5,17 @@ function Header() {
         <header className="header">
             <a href="#" className='header__logo'><img src="../src/assets/img/logoCodeChella.svg"></img></a>
 
-            <a href='#' className='header__menu'><img src='../src/assets/img/menu.svg'></img></a>
+            <img src='../src/assets/img/menu.svg' className='header__menu'></img>
 
-            <nav className="header__nav">
-                <i className="fa-solid fa-bars nav__icone-menu"></i>
-                <ul className="nav__lista hidden">
-                    <li className="nav__item">
-                        <a href="experiencia.html" className="p-bold nav__link">A experiência</a>
-                        <i className="nav__barra"></i>
-                    </li>
-                    <li className="nav__item">
-                        <a href="setores.html" className="p-bold nav__link">Mapa de Setores</a>
-                        <i className="nav__barra"></i>
-                    </li>
-                    <li className="nav__item">
-                        <a href="informacoes.html" className="p-bold nav__link">Informações</a>
-                        <i className="nav__barra"></i>
-                    </li>
-                    <li className="nav__item">
-                        <a href="ingresso.html" className="p-bold nav__link">Ingresso</a>
-                    </li>
-                </ul>
+            <nav className="vertical-menu">
+                <a href="#" className="active"></a>
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+                <a href="#">Link 4</a>
             </nav>
+
+            
         </header>
     )
 }
