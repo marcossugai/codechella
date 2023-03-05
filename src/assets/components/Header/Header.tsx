b0819b531afe6e31e9f0a2da0b0561deba0a3b3e
Marcos Sugai Ogama <pazzino2000@yahoo.it>
2023-03-05 16:23:40 -0300

    mod 20230304

--- src/assets/components/Header/Header.tsx
+++ src/assets/components/Header/Header.tsx
@@ -1,20 +1,40 @@
+import { useState } from 'react'
 import './Header.css'
 
 function Header() {
+const CustomNav = ({ li }) => {
+    const [window, setWindow] = useState(false);
+
+    let openClose = () =>
+    if (window === false) {setWindow(true)};
+    else {setWindow(false)};
+
     return(
         <header className="header">
             <a href="#" className='header__logo'><img src="../src/assets/img/logoCodeChella.svg"></img></a>
 
-            <img src='../src/assets/img/menu.svg' className='header__menu'></img>
-
-            <nav className="vertical-menu">
-                <a href="#" className="active"></a>
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-                <a href="#">Link 4</a>
+            <nav className='navbar-menu' style={{ width: window === false ? 250 : 60 }}>
+                <div className='burger' onClick={() => openClose()}>
+                    <img src='.img/menu.svg' alt='burger' />
+                </div>
+                <ul className='navbar__list'>
+                    {li.map((item, i) => (
+                        <div className='navbar__li-box' key={i}>
+                            <img
+                            src={item[1]}
+                            alt={item[1]}
+                            style={{ paddingLeft: window ===false ? 27 : 17}}
+                            />
+                            <li
+                            className='navbar__li'
+                            style={{ display: window ===false ? | "inline-blcok" ; "none"}}
+                            >
+                                {item[0]}
+                            </li>
+                        </div>
+                    ))}
+                </ul>
             </nav>
-
             
         </header>
     )
